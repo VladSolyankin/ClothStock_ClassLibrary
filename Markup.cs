40eafd88471e829d24ad47a91a5f8babe26abdcb
Владислав Солянкин <vlad.solyankin97@mail.ru>
2022-05-19 22:46:04 +0300

    Added Markup enum

---
+++ Markup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothStock_ClassLibrary
+{
+    public enum Markup
+    {
+        три, пять, десять, пятнадцать
+    }
+}
